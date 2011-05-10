25ca3fa08ee5c53119381fe77e142e5e1250c94b
SebastianDotNet <SebastianDotNet@codeplex.com>
2011-05-10 17:38:59 +0000

    - add tutorial07
    Imported from NetOffice r73013
    https://netoffice.codeplex.com/SourceControl/changeset/73013

--- Net2.0/Tutorials/Tutorial07/Form1.Designer.cs
+++ Net2.0/Tutorials/Tutorial07/Form1.Designer.cs
@@ -28,13 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBoxInfo = new System.Windows.Forms.RichTextBox();
+            this.buttonStartExample = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // richTextBoxInfo
+            // 
+            this.richTextBoxInfo.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.richTextBoxInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBoxInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxInfo.Location = new System.Drawing.Point(12, 77);
+            this.richTextBoxInfo.Name = "richTextBoxInfo";
+            this.richTextBoxInfo.Size = new System.Drawing.Size(801, 237);
+            this.richTextBoxInfo.TabIndex = 12;
+            this.richTextBoxInfo.Text = resources.GetString("richTextBoxInfo.Text");
+            // 
+            // buttonStartExample
+            // 
+            this.buttonStartExample.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStartExample.Location = new System.Drawing.Point(12, 32);
+            this.buttonStartExample.Name = "buttonStartExample";
+            this.buttonStartExample.Size = new System.Drawing.Size(801, 30);
+            this.buttonStartExample.TabIndex = 11;
+            this.buttonStartExample.Text = "Start example";
+            this.buttonStartExample.UseVisualStyleBackColor = true;
+            this.buttonStartExample.Click += new System.EventHandler(this.buttonStartExample_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(683, 273);
+            this.ClientSize = new System.Drawing.Size(832, 349);
+            this.Controls.Add(this.richTextBoxInfo);
+            this.Controls.Add(this.buttonStartExample);
             this.Name = "Form1";
             this.Text = "Tutorial07 - Invoker";
             this.ResumeLayout(false);
@@ -42,6 +69,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxInfo;
+        private System.Windows.Forms.Button buttonStartExample;
     }
 }
 
