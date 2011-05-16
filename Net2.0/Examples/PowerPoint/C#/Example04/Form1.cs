66fe8b142bd52676e580e5c4fb95bc2512b92c01
SebastianDotNet <SebastianDotNet@codeplex.com>
2011-05-16 22:51:09 +0000

    - add c# power point examples
    Imported from NetOffice r73583
    https://netoffice.codeplex.com/SourceControl/changeset/73583

---
+++ Net2.0/Examples/PowerPoint/C#/Example04/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using LateBindingApi.Core;
+using PowerPoint = NetOffice.PowerPointApi;
+using NetOffice.PowerPointApi.Enums;
+using NetOffice.OfficeApi.Enums; 
+
+namespace Example04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Initialize Api COMObject Support
+            LateBindingApi.Core.Factory.Initialize();
+
+            // start word and turn off msg boxes
+            PowerPoint.Application powerApplication = new PowerPoint.Application();
+            powerApplication.DisplayAlerts = PpAlertLevel.ppAlertsNone;
+
+            // add a new presentation with two new slides
+            PowerPoint.Presentation presentation = powerApplication.Presentations.Add(MsoTriState.msoTrue);
+            PowerPoint.Slide slide1 = presentation.Slides.Add(1, PpSlideLayout.ppLayoutBlank);
+            PowerPoint.Slide slide2 = presentation.Slides.Add(1, PpSlideLayout.ppLayoutBlank);
+
+            // add shapes
+            slide1.Shapes.AddShape(MsoAutoShapeType.msoShape4pointStar, 100, 100, 200, 200);
+            slide2.Shapes.AddShape(MsoAutoShapeType.msoShapeDoubleWave, 200, 200, 200, 200);
+
+            // change blend animation
+            slide1.SlideShowTransition.EntryEffect = PpEntryEffect.ppEffectCoverDown;
+            slide1.SlideShowTransition.Speed = PpTransitionSpeed.ppTransitionSpeedFast;
+            
+            slide2.SlideShowTransition.EntryEffect = PpEntryEffect.ppEffectCoverLeftDown;
+            slide2.SlideShowTransition.Speed = PpTransitionSpeed.ppTransitionSpeedFast;
+
+            // save the document 
+            string fileExtension = GetDefaultExtension(powerApplication);
+            string documentFile = string.Format("{0}\\Example04{1}", Environment.CurrentDirectory, fileExtension);
+            presentation.SaveAs(documentFile, PpSaveAsFileType.ppSaveAsDefault, MsoTriState.msoTrue);
+
+            // close power point and dispose reference
+            powerApplication.Quit();
+            powerApplication.Dispose();
+
+            FinishDialog fDialog = new FinishDialog("Presentation saved.", (string)documentFile);
+            fDialog.ShowDialog(this);
+        }
+
+        #region Helper
+
+        /// <summary>
+        /// returns the valid file extension for the instance. for example ".ppt" or ".pptx"
+        /// </summary>
+        /// <param name="application">the instance</param>
+        /// <returns>the extension</returns>
+        private static string GetDefaultExtension(PowerPoint.Application application)
+        {
+            double Version = Convert.ToDouble(application.Version);
+            if (Version >= 120.00)
+                return ".pptx";
+            else
+                return ".ppt";
+        }
+
+        #endregion
+    }
+}
---
+++ Net2.0/Examples/PowerPoint/C#/Example06/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using LateBindingApi.Core;
+using PowerPoint = NetOffice.PowerPointApi;
+using NetOffice.PowerPointApi.Enums;
+using NetOffice.OfficeApi.Enums;
+
+namespace Example06
+{
+    public partial class Form1 : Form
+    {
+        private delegate void UpdateEventTextDelegate(string Message);
+        UpdateEventTextDelegate _updateDelegate;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _updateDelegate = new UpdateEventTextDelegate(UpdateTextbox);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Initialize Api COMObject Support
+            LateBindingApi.Core.Factory.Initialize();
+
+            // start word and turn off msg boxes
+            PowerPoint.Application powerApplication = new PowerPoint.Application();
+            powerApplication.DisplayAlerts = PpAlertLevel.ppAlertsNone;
+
+            /*
+            we register some events. note: the event trigger was called from power point, means an other Thread
+            remove the Quit() call below and check out more events if you want
+            */
+
+            powerApplication.PresentationCloseEvent += new NetOffice.PowerPointApi.Application_PresentationCloseEventHandler(powerApplication_PresentationCloseEvent);
+            powerApplication.AfterNewPresentationEvent += new NetOffice.PowerPointApi.Application_AfterNewPresentationEventHandler(powerApplication_AfterNewPresentationEvent);
+             
+            // add a new presentation with one new slide
+            PowerPoint.Presentation presentation = powerApplication.Presentations.Add(MsoTriState.msoTrue);
+            PowerPoint.Slide slide = presentation.Slides.Add(1, PpSlideLayout.ppLayoutBlank);
+                     
+            // close the document
+            presentation.Close();
+
+            // close power point and dispose reference
+            powerApplication.Quit();
+            powerApplication.Dispose();
+        }
+
+      
+        void powerApplication_PresentationCloseEvent(NetOffice.PowerPointApi.Presentation Pres)
+        {
+            textBoxEvents.BeginInvoke(_updateDelegate, new object[] { "Event PresentationClose called." });
+            Pres.Dispose();
+        }
+
+        void powerApplication_AfterNewPresentationEvent(NetOffice.PowerPointApi.Presentation Pres)
+        {
+            textBoxEvents.BeginInvoke(_updateDelegate, new object[] { "Event AfterNewPresentation called." });
+            Pres.Dispose();
+        }
+
+        private void UpdateTextbox(string message)
+        {
+            textBoxEvents.AppendText(message + "\r\n");
+        }
+    }
+}
