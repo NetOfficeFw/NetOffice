26e77e0d17595ec49033cc35daa37663caf11119
SebastianDotNet <SebastianDotNet@codeplex.com>
2011-05-25 16:45:18 +0000

    - add 6 c# outlook examples
    Imported from NetOffice r74378
    https://netoffice.codeplex.com/SourceControl/changeset/74378

---
+++ Net2.0/Examples/Outlook/C#/Example02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Example02
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ Net2.0/Examples/Outlook/C#/Example04/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using LateBindingApi.Core;
+using Outlook = NetOffice.OutlookApi;
+using NetOffice.OutlookApi.Enums; 
+
+namespace Example04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Initialize Api COMObject Support
+            LateBindingApi.Core.Factory.Initialize();
+
+            // Create an Outlook Application object. 
+            Outlook.Application outlookApplication = new Outlook.Application();
+            
+            // one simple call
+            outlookApplication.Session.SendAndReceive(false);
+            outlookApplication.GetNamespace("MAPI").SendAndReceive(false);
+
+            // close outlook and dispose
+            outlookApplication.Quit();
+            outlookApplication.Dispose();
+
+            MessageBox.Show(this, "Done!", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
