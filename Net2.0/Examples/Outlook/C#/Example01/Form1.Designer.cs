2c2f4efec2089e0b488e25d2e4b451613cc77081
SebastianDotNet <SebastianDotNet@codeplex.com>
2011-05-25 11:08:55 +0000

    - add outlook example
    Imported from NetOffice r74354
    https://netoffice.codeplex.com/SourceControl/changeset/74354

---
+++ Net2.0/Examples/Outlook/C#/Example01/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Example1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.labelItemsCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(22, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(477, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start example";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox1.Location = new System.Drawing.Point(22, 67);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(477, 118);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "this example shows you how to enumerate your inbox folder";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Location = new System.Drawing.Point(22, 214);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(477, 140);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "From";
+            this.columnHeader1.Width = 130;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Subject";
+            this.columnHeader2.Width = 300;
+            // 
+            // labelItemsCount
+            // 
+            this.labelItemsCount.AutoSize = true;
+            this.labelItemsCount.Location = new System.Drawing.Point(19, 198);
+            this.labelItemsCount.Name = "labelItemsCount";
+            this.labelItemsCount.Size = new System.Drawing.Size(0, 13);
+            this.labelItemsCount.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 378);
+            this.Controls.Add(this.labelItemsCount);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Example01 - Inbox";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Label labelItemsCount;
+    }
+}
+
---
+++ Net2.0/Examples/Outlook/C#/Example01/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Reflection;
+using System.Drawing;
+using System.Windows.Forms;
+
+using LateBindingApi.Core;
+using Outlook = NetOffice.OutlookApi;
+using NetOffice.OutlookApi.Enums; 
+
+namespace Example1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+  
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Initialize Api COMObject Support
+            LateBindingApi.Core.Factory.Initialize();
+ 
+            // start outlook
+            Outlook.Application outlookApplication = new Outlook.Application();
+            
+            // Get inbox 
+            Outlook._NameSpace outlookNS = outlookApplication.GetNamespace("MAPI");
+            Outlook.MAPIFolder inboxFolder = outlookNS.GetDefaultFolder(OlDefaultFolders.olFolderInbox);
+
+            // setup gui
+            listView1.Items.Clear();
+            labelItemsCount.Text = string.Format("You have {0} e-mails.", inboxFolder.Items.Count);
+            
+            // we fetch the inbox folder items
+            Outlook._Items items = inboxFolder.Items;
+            COMObject item = null;
+            int i = 1;
+            do
+            {
+                if(null == item)
+                    item = items.GetFirst();
+
+                // not every item is a mail item
+                Outlook.MailItem mailItem = item as Outlook.MailItem;
+                if (null != mailItem)
+                {
+                    //Console.WriteLine(i.ToString() + " " + mailItem.SenderName);
+                    ListViewItem newItem = listView1.Items.Add(mailItem.SenderName);
+                    newItem.SubItems.Add(mailItem.Subject);
+                }
+                item.Dispose();
+                item = items.GetNext();
+                i++;
+            } while (null != item);
+
+            // close outlook and dispose
+            outlookApplication.Quit();
+            outlookApplication.Dispose();
+        }
+
+    }
+}
