d3c0e9248140a01eb1d47009215f7ad75270da06
SebastianDotNet <SebastianDotNet@codeplex.com>
2011-05-31 09:17:58 +0000

    - update LateBindingApi.Core.dll - fix issue 15922 (AccessViolationException) once and for all - change release tag
    Imported from NetOffice r74716
    https://netoffice.codeplex.com/SourceControl/changeset/74716

--- Source/Net v4.0/LateBindingApi.Core/Factory.cs
+++ Source/Net v4.0/LateBindingApi.Core/Factory.cs
@@ -277,23 +277,22 @@ namespace LateBindingApi.Core
         /// <returns></returns>
         private static Guid GetParentLibraryGuid(object comProxy)
         {
-            Guid returnGuid = Guid.Empty; 
-
+        Guid returnGuid = Guid.Empty; 
+        
             IDispatch dispatcher = (IDispatch)comProxy;
             COMTypes.ITypeInfo typeInfo = dispatcher.GetTypeInfo(0, 0);
             COMTypes.ITypeLib parentTypeLib = null;
-
+ 
             int i = 0;
             typeInfo.GetContainingTypeLib(out parentTypeLib, out i);
-            
+ 
             IntPtr attributesPointer = IntPtr.Zero;
             parentTypeLib.GetLibAttr(out attributesPointer);
-           
-            COMTypes.TYPEATTR attributes = (COMTypes.TYPEATTR)Marshal.PtrToStructure(attributesPointer, typeof(COMTypes.TYPEATTR));
-            returnGuid = attributes.guid;
 
-            Marshal.FreeHGlobal(attributesPointer); 
-            //parentTypeLib.ReleaseTLibAttr(attributesPointer);
+            COMTypes.TYPELIBATTR attributes = (COMTypes.TYPELIBATTR)Marshal.PtrToStructure(attributesPointer, typeof(COMTypes.TYPELIBATTR));
+            returnGuid = attributes.guid;
+                  
+            parentTypeLib.ReleaseTLibAttr(attributesPointer);
             Marshal.ReleaseComObject(parentTypeLib);
             Marshal.ReleaseComObject(typeInfo);
 
