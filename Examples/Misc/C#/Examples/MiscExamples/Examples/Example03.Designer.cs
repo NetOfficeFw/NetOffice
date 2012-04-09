8d892abcb0fe880bdf062b44c1c106618ada50d5
SebastianDotNet <SebastianDotNet@codeplex.com>
2012-04-09 14:41:09 +0000

    - add new misc examples
    Imported from NetOffice r89031
    https://netoffice.codeplex.com/SourceControl/changeset/89031

---
+++ Examples/Misc/C#/Examples/MiscExamples/Examples/Example03.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace MiscExamplesCS4
+{
+    partial class Example03
+    {
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Example03));
+            this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.buttonStartExample = new System.Windows.Forms.Button();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.labelEventLogHeader = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxDescription.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxDescription.Location = new System.Drawing.Point(28, 72);
+            this.textBoxDescription.Multiline = true;
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.Size = new System.Drawing.Size(679, 27);
+            this.textBoxDescription.TabIndex = 19;
+            this.textBoxDescription.Text = "This example shows you how to access a running Outlook application.";
+            // 
+            // buttonStartExample
+            // 
+            this.buttonStartExample.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStartExample.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStartExample.Image = ((System.Drawing.Image)(resources.GetObject("buttonStartExample.Image")));
+            this.buttonStartExample.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonStartExample.Location = new System.Drawing.Point(28, 23);
+            this.buttonStartExample.Name = "buttonStartExample";
+            this.buttonStartExample.Size = new System.Drawing.Size(679, 28);
+            this.buttonStartExample.TabIndex = 18;
+            this.buttonStartExample.Text = "Start example";
+            this.buttonStartExample.UseVisualStyleBackColor = true;
+            this.buttonStartExample.Click += new System.EventHandler(this.buttonStartExample_Click);
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBoxLog.Location = new System.Drawing.Point(28, 141);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.Size = new System.Drawing.Size(679, 139);
+            this.textBoxLog.TabIndex = 20;
+            // 
+            // labelEventLogHeader
+            // 
+            this.labelEventLogHeader.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelEventLogHeader.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelEventLogHeader.Location = new System.Drawing.Point(28, 116);
+            this.labelEventLogHeader.Name = "labelEventLogHeader";
+            this.labelEventLogHeader.Size = new System.Drawing.Size(679, 22);
+            this.labelEventLogHeader.TabIndex = 21;
+            this.labelEventLogHeader.Text = "EventLog";
+            // 
+            // Example03
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.labelEventLogHeader);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.textBoxDescription);
+            this.Controls.Add(this.buttonStartExample);
+            this.Name = "Example03";
+            this.Size = new System.Drawing.Size(739, 304);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxDescription;
+        private System.Windows.Forms.Button buttonStartExample;
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.Label labelEventLogHeader;
+    }
+}
---
+++ Examples/Misc/VB/COMAddin Examples/SuperAddin/OfficeRegistry.vb
@@ -0,0 +1,83 @@
+﻿Imports Microsoft.Win32
+
+''' <summary>
+''' office addin keys and create/delete methods
+''' </summary>
+''' <remarks></remarks>
+Public Class OfficeRegistry
+
+    '
+    ' office addin registry keys 
+    '
+
+    Public Shared Office As String = "Software\\Microsoft\\Office\\"
+    Public Shared Excel As String = "Software\\Microsoft\\Office\\Excel\\AddIns\\"
+    Public Shared Word As String = "Software\\Microsoft\\Office\\Word\\AddIns\\"
+    Public Shared Outlook As String = "Software\\Microsoft\\Office\\Outlook\\AddIns\\"
+    Public Shared PowerPoint As String = "Software\\Microsoft\\Office\\PowerPoint\\AddIns\\"
+    Public Shared Access As String = "Software\\Microsoft\\Office\\Access\\AddIns\\"
+
+    ''' <summary>
+    ''' creates addin key
+    ''' </summary>
+    ''' <param name="officeApp"></param>
+    ''' <param name="progId"></param>
+    ''' <param name="name"></param>
+    ''' <param name="description"></param>
+    ''' <remarks></remarks>
+    Public Shared Sub CreateAddinKey(ByVal officeApp As String, ByVal progId As String, ByVal name As String, ByVal description As String)
+
+        Dim regKey As String = GetRegistryKey(officeApp)
+
+        Dim rk As RegistryKey = Registry.CurrentUser.CreateSubKey(regKey + progId)
+        rk.Close()
+        rk = Registry.CurrentUser.OpenSubKey(regKey + progId, True)
+        rk.SetValue("LoadBehavior", Convert.ToInt32(3))
+        rk.SetValue("FriendlyName", name)
+        rk.SetValue("Description", description)
+
+    End Sub
+
+    ''' <summary>
+    ''' deletes addin key
+    ''' </summary>
+    ''' <param name="key"></param>
+    ''' <remarks></remarks>
+    Public Shared Sub DeleteAddinKey(ByVal key As String)
+
+        Registry.CurrentUser.DeleteSubKey(key, False)
+
+    End Sub
+
+    Public Shared Sub LogErrorMessage(ByVal officeApp As String, ByVal progId As String, ByVal message As String, ByVal exception As Exception)
+
+        Dim regKey As String = GetRegistryKey(officeApp)
+        Dim rk As RegistryKey = Registry.CurrentUser.OpenSubKey(regKey + progId, True)
+
+        rk.SetValue("ErrorTimestamp", DateTime.Now.ToString())
+        rk.SetValue("ErrorMessage", message)
+        rk.SetValue("ErrorException", exception.Message)
+        rk.Close()
+
+    End Sub
+
+    Private Shared Function GetRegistryKey(ByVal officeApp As String) As String
+
+        Select Case officeApp
+            Case "Excel"
+                Return Excel
+            Case "Word"
+                Return Word
+            Case "Outlook"
+                Return Outlook
+            Case "PowerPoint"
+                Return PowerPoint
+            Case "Access"
+                Return Access
+            Case Else
+                Throw New ArgumentOutOfRangeException("officeApp")
+        End Select
+
+    End Function
+
+End Class
