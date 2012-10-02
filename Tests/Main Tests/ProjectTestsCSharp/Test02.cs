f6ba660deacd9bd1318a5d5804b791cb40513b4b
SebastianDotNet <SebastianDotNet@codeplex.com>
2012-10-02 20:16:50 +0000

    - add 2 msproject tests (C# and VB)
    Imported from NetOffice r94902
    https://netoffice.codeplex.com/SourceControl/changeset/94902

---
+++ Tests/Main Tests/ProjectTestsCSharp/Test02.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tests.Core;
+using MSProject = NetOffice.MSProjectApi;
+using NetOffice.MSProjectApi.Enums;
+
+namespace ProjectTestsCSharp
+{
+    public class Test02 : ITestPackage
+    {
+        #region TestPackage Member
+
+        public string Name
+        {
+            get { return "Test02"; }
+        }
+
+        public string Description
+        {
+            get { return "Add a new project and add 10 tasks."; }
+        }
+
+        public string OfficeProduct
+        {
+            get { return "Project"; }
+        }
+
+        public string Language
+        {
+            get { return "C#"; }
+        }
+
+        public TestResult DoTest()
+        {
+            MSProject.Application application = null;
+            DateTime startTime = DateTime.Now;
+            try
+            {
+                application = new MSProject.Application();
+
+                application.ProjectTaskNewEvent += new MSProject.Application_ProjectTaskNewEventHandler(ApplicationProjectTaskNewEvent);
+                application.ProjectBeforeCloseEvent += new MSProject.Application_ProjectBeforeCloseEventHandler(ApplicationProjectBeforeCloseEvent);
+                application.ProjectBeforeTaskChangeEvent += new MSProject.Application_ProjectBeforeTaskChangeEventHandler(ApplicationProjectBeforeTaskChangeEvent);
+                application.ProjectBeforeTaskDeleteEvent += new MSProject.Application_ProjectBeforeTaskDeleteEventHandler(ApplicationProjectBeforeTaskDeleteEvent);
+               
+                MSProject.Project newProject = application.Projects.Add();
+                MSProject.Task task1 = newProject.Tasks.Add("Task 1");
+                MSProject.Task task2 =  newProject.Tasks.Add("Task 2");
+                
+                task2.Delete();
+
+                application.FileCloseAll(false);
+
+                if (TaskDeleteEventCalled && TaskChangeEventCalled && BeforeCloseEventCalled && TaskChangeEventCalled)
+                    return new TestResult(true, DateTime.Now.Subtract(startTime), "", null, "");
+                else 
+                {
+                    string errorMessage = "";
+                    if (!TaskDeleteEventCalled)
+                        errorMessage += "ProjectBeforeTaskDeleteEvent failed ";
+                    if (!TaskChangeEventCalled)
+                        errorMessage += "ProjectBeforeTaskChangeEvent failed ";
+                    if (!BeforeCloseEventCalled)
+                        errorMessage += "ProjectBeforeCloseEvent failed ";
+                    if (!TaskNewEventCalled)
+                        errorMessage += "ProjectTaskNewEvent failed ";
+
+                    return new TestResult(false, DateTime.Now.Subtract(startTime), errorMessage, null, "");
+                }
+            }
+            catch (Exception exception)
+            {
+                return new TestResult(false, DateTime.Now.Subtract(startTime), exception.Message, exception, "");
+            }
+            finally
+            {
+                if (null != application)
+                {
+                    application.Quit(false);
+                    application.Dispose();
+                }
+            }
+        }
+
+        #endregion
+
+        private bool TaskDeleteEventCalled { get; set; }
+        private bool TaskChangeEventCalled { get; set; }
+        private bool BeforeCloseEventCalled { get; set; }
+        private bool TaskNewEventCalled { get; set; }
+
+
+        void ApplicationProjectBeforeTaskDeleteEvent(MSProject.Task tsk, ref bool Cancel)
+        {
+            TaskDeleteEventCalled = true;
+        }
+
+        void ApplicationProjectBeforeTaskChangeEvent(MSProject.Task tsk, PjField Field, object NewVal, ref bool Cancel)
+        {
+            TaskChangeEventCalled = true;
+        }
+
+        void ApplicationProjectBeforeCloseEvent(MSProject.Project pj, ref bool Cancel)
+        {
+            BeforeCloseEventCalled = true;
+        }
+
+        void ApplicationProjectTaskNewEvent(MSProject.Project pj, int ID)
+        {
+            TaskNewEventCalled = true;
+        }
+    }
+}
---
+++ Tests/Main Tests/ProjectTestsVB/TestAssembly.vb
@@ -0,0 +1,36 @@
+﻿Imports Tests.Core
+
+Public Class TestAssembly
+    Implements ITestAssembly
+
+    Private _listPackages As List(Of ITestPackage)
+
+    Public ReadOnly Property Language As String Implements Tests.Core.ITestAssembly.Language
+        Get
+            Return "VB"
+        End Get
+    End Property
+
+    Public Function LoadTestPackages() As Tests.Core.ITestPackage() Implements Tests.Core.ITestAssembly.LoadTestPackages
+
+        If IsNothing(_listPackages) Then
+
+            NetOffice.DebugConsole.Mode = NetOffice.ConsoleMode.Console
+
+            _listPackages = New List(Of ITestPackage)
+            _listPackages.Add(New Test01())
+            _listPackages.Add(New Test02())
+
+        End If
+
+        Return _listPackages.ToArray()
+
+    End Function
+
+    Public ReadOnly Property OfficeProduct As String Implements Tests.Core.ITestAssembly.OfficeProduct
+        Get
+            Return "Project"
+        End Get
+    End Property
+
+End Class
