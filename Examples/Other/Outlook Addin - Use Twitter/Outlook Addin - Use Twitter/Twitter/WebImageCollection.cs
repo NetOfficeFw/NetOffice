3634f2ce0017c54a8ea70a927591506762a8ef9a
SebastianDotNet <SebastianDotNet@codeplex.com>
2012-10-19 05:45:51 +0000

    - add pong game to twitter addin
    Imported from NetOffice r95421
    https://netoffice.codeplex.com/SourceControl/changeset/95421

---
+++ Examples/Other/Outlook Addin - Use Twitter/Outlook Addin - Use Twitter/Twitter/WebImageCollection.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Web;
+using System.Drawing;
+using System.IO;
+
+namespace Sample.Addin
+{
+    internal class WebImage
+    {
+        internal WebImage(string url, Image image)
+        {
+            Url = url;
+            Image = image;
+        }
+
+        internal string Url { get; private set; }
+        internal Image Image { get; private set; }
+    }
+
+    internal class WebImageCollection
+    {
+        public WebImageCollection()
+        {
+            Images = new List<WebImage>();
+        }
+        private List<WebImage> Images { get; set; }
+
+        public Image this[string url]
+        {
+            get
+            {
+                foreach (WebImage item in Images)
+                {
+                    if (item.Url.Equals(url, StringComparison.InvariantCultureIgnoreCase))
+                        return item.Image;
+                }
+
+                Image newImage = GetImageFromURL(url);
+                Images.Add(new WebImage(url, newImage));
+                return newImage;
+            }
+        }
+
+        /// <summary>
+        /// Gets the image from URL.
+        /// </summary>
+        /// <param name="url">The URL.</param>
+        /// <returns></returns>
+        private static Image GetImageFromURL(string url)
+        {
+            try
+            {
+                HttpWebRequest httpWebRequest = (HttpWebRequest)HttpWebRequest.Create(url);
+                HttpWebResponse httpWebReponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                Stream stream = httpWebReponse.GetResponseStream();
+                return Image.FromStream(stream);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+    }
+}
---
+++ Examples/Other/Outlook Addin - Use Twitter/Outlook Addin - Use Twitter/UserControls/ErrorPane.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sample.Addin
+{
+    public partial class ErrorPane : UserControl
+    {
+        public ErrorPane()
+        {
+            InitializeComponent();
+        }
+
+        public void ShowError(Exception exception)
+        {
+            pictureBox2.Visible = true;
+            labelErrorMessage.Text = exception.Message;
+            labelErrorMessage.Visible = true;
+            buttonErrorDetails.Visible = true;
+        }
+
+        public void ClearError()
+        {
+            pictureBox2.Visible = false;
+            labelErrorMessage.Text = string.Empty;
+            labelErrorMessage.Visible = false;
+            buttonErrorDetails.Visible = false;
+        }
+    }
+}
