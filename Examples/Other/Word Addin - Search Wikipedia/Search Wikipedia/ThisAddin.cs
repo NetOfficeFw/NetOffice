3b5f4b53903108a10cae2c4b5298160f65d0a685
SebastianDotNet <SebastianDotNet@codeplex.com>
2012-10-25 11:06:04 +0000

    - fix visio keytoken bug
    Imported from NetOffice r95603
    https://netoffice.codeplex.com/SourceControl/changeset/95603

--- Examples/Other/Word Addin - Search Wikipedia/Search Wikipedia/ThisAddin.cs
+++ Examples/Other/Word Addin - Search Wikipedia/Search Wikipedia/ThisAddin.cs
@@ -13,7 +13,7 @@ namespace Sample.Addin
     /// The main addin for MS-Word. The Addin use the base class COMAddin from NetOffice.WordApi.Tools.
     /// Learn more about the NetOffice Tools namespace: http://netoffice.codeplex.com/wikipage?title=Tools_EN
     /// </summary>
-    [GuidAttribute("56F843AD-ECB8-45D6-9E33-C0928BD2FB03"), ProgId("Sample.WordAddin"), ComVisible(true)]
+    [GuidAttribute("56F843AD-ECB8-45D6-9E33-C0928BD2FB03"), ProgId("Sample.WordAddin")]
     [COMAddin("Word Wikipedia Addin", "This Addin provides Wikipedia functionality", 3)]
     public class ThisAddin : COMAddin
     {
@@ -25,6 +25,20 @@ namespace Sample.Addin
             TaskPanes[0].DockPositionRestrict = MsoCTPDockPositionRestrict.msoCTPDockPositionRestrictNoHorizontal;
             TaskPanes[0].Width = 520;
             TaskPanes[0].Visible = true;
+
+            this.OnConnection += new OnConnectionEventHandler(ThisAddin_OnConnection);
+            this.OnStartupComplete += new OnStartupCompleteEventHandler(ThisAddin_OnStartupComplete);
+        }
+
+        void ThisAddin_OnConnection(object Application, ext_ConnectMode ConnectMode, object AddInInst, ref Array custom)
+        {
+
+        }
+
+        [RegisterFunction(RegisterMode.CallAfter)]
+        void ThisAddin_OnStartupComplete(ref Array custom)
+        {
+            
         }
     }
 }
