45a24b489926d5bca8a75d7995e90d98e8ea489f
SebastianDotNet <SebastianDotNet@codeplex.com>
2013-10-26 12:42:40 +0000

    [Bugfix Commit] -upate source/assemblies NetOffice.dll 1.6.1 to 1.7.0
    Imported from NetOffice r104147
    https://netoffice.codeplex.com/SourceControl/changeset/104147

--- Tools/NOTools.CSharpTextEditor/ErrorDoubleClickEventHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace NOTools.CSharpTextEditor
-{
-    internal delegate void ErrorDoubleClickEventHandler(ErrorPanel sender, int lineNumber, int columnNumber);
-}
