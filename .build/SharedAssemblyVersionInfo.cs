9a15548a6b4f117107bcaffe371d7db64c0473e0
Jozef Izso <jozef.izso@gmail.com>
2016-10-15 23:37:40 +0200

    Update version information

--- .build/SharedAssemblyVersionInfo.cs
+++ .build/SharedAssemblyVersionInfo.cs
@@ -5,4 +5,4 @@
 
 // This is used for the NuSpec version tag replacement
 // and is combined with nuget-specific rev and release
-[assembly: AssemblyInformationalVersion("1.7.4")]
+[assembly: AssemblyInformationalVersion("1.7.4-dev")]
