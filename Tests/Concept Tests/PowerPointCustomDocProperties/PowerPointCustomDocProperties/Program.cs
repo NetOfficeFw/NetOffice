184a43e0d97d42dad841b4152502b50b909dfc97
SebastianDotNet <microschuft@hotmail.com>
2018-03-14 20:00:23 +0000

    ﻿- modify PowerPointCustomDocProperties
    Imported from NetOffice r54
    https://osdn.net/projects/netoffice/scm/svn/commits/54

--- Tests/Concept Tests/PowerPointCustomDocProperties/PowerPointCustomDocProperties/Program.cs
+++ Tests/Concept Tests/PowerPointCustomDocProperties/PowerPointCustomDocProperties/Program.cs
@@ -7,6 +7,7 @@ namespace PowerPointCustomDocProperties
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
             try
--- Tests/Concept Tests/PowerPointCustomDocProperties/PowerPointCustomDocProperties/Test01.cs
+++ Tests/Concept Tests/PowerPointCustomDocProperties/PowerPointCustomDocProperties/Test01.cs
@@ -13,28 +13,31 @@ namespace PowerPointCustomDocProperties
 {
     public class Test01
     {
-        public NetOffice.Core Proceed(int testNumber)
+        public void Proceed(int testNumber)
         {
             using (var application = COMObject.Create<PowerPoint.Application>(COMObjectCreateOptions.CreateNewCore))
             {
-                application.Console.OnException += delegate (DebugConsole arg1, Exception arg2)
+                application.Settings.EnableAutomaticQuit = true;
+                application.Settings.ExceptionMessageBehavior = ExceptionMessageHandling.DiagnosticsAndInnerMessage;
+                application.Settings.ExceptionDiagnosticsMessage = "Failed to proceed {CallInstance}={CallType}=>{Name}{ParenthesizedArgs}.";
+
+                application.Console.OnException += delegate(DebugConsole sender, Exception error)
                 {
-                    Console.WriteLine("Test{0}, Console_OnException: {1}", testNumber, arg2.Message);
+                    Console.WriteLine("Test{0}, Console_OnException: {1}", testNumber, error.Message);
                 };
 
                 application.DisplayAlerts = PpAlertLevel.ppAlertsNone;
                 application.Visible = MsoTriState.msoCTrue;
-                application.Settings.EnableAutomaticQuit = true;
-                application.Settings.ExceptionMessageBehavior = ExceptionMessageHandling.DiagnosticsAndInnerMessage;
 
                 var presentation = application.Presentations.Add();
                 var properties = presentation.Property<Office.DocumentProperties>("CustomDocumentProperties");
 
-                var randomValue = new Random().Next(1, 999);
-
-                properties.Add("CustomProperty1", false, MsoDocProperties.msoPropertyTypeNumber, randomValue);
-
-                return application.Factory;
+                properties.Add(
+                    "CustomProperty1",
+                    false,
+                    MsoDocProperties.msoPropertyTypeNumber,
+                    new Random().Next(1, 999)
+                    );
             }
         }
     }
