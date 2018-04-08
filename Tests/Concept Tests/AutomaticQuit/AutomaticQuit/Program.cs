16b0176e6f84acf1c1fa193b5f79295e738fb838
SebastianDotNet <microschuft@hotmail.com>
2018-04-08 20:54:36 +0000

    ﻿- add automatic quit concept test
    Imported from NetOffice r71
    https://osdn.net/projects/netoffice/scm/svn/commits/71

---
+++ Tests/Concept Tests/AutomaticQuit/AutomaticQuit/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NetOffice;
+using Office = NetOffice.OfficeApi;
+using NetOffice.OfficeApi.Enums;
+using Excel = NetOffice.ExcelApi;
+using NetOffice.ExcelApi.Enums;
+using NetOffice.Extensions;
+
+namespace AutomaticQuit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Core core = new Core();
+            core.Settings.ForceApplicationVersionProviders = false;
+            core.Settings.EnableAutomaticQuit = true;
+
+            using (var application = new Excel.Application(core))
+            {
+                application.Visible = true;
+                var sheet = application.
+                                Workbooks.
+                                Add().
+                                Worksheets[1].
+                                SetProperty<Excel.Worksheet>("Name", "MyWorksheet");
+
+                application.Quit();
+                core.Invoker.Method(application, "Foo123");
+            }
+        }
+    }
+}
