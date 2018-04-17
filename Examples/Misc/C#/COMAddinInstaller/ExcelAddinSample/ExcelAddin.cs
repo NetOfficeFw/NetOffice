0077372bdd9035481d8fc7a23932a5a36a21a87f
Jozef Izso <jozef.izso@gmail.com>
2018-04-17 16:25:25 +0000

    ﻿- add example installer that registers Excel add-in (32-bit only)
    Imported from NetOffice r74
    https://osdn.net/projects/netoffice/scm/svn/commits/74

---
+++ Examples/Misc/C#/COMAddinInstaller/ExcelAddinSample/ExcelAddin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using NetOffice.ExcelApi.Tools;
+
+namespace ExcelAddinSample
+{
+    [ProgId("NetOffice.ExcelAddinInstaller")]
+    [Guid("121A6EA7-AD5F-471B-A750-F4D6280472C0")]
+    public class ExcelAddin : COMAddin
+    {
+
+        public ExcelAddin()
+        {
+            this.OnStartupComplete += ExcelAddin_OnStartupComplete;
+        }
+
+        private void ExcelAddin_OnStartupComplete(ref Array custom)
+        {
+            MessageBox.Show($"NetOffice Add-in running in Microsoft Excel {this.Application.Version}", "NetOffice Add-in", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
