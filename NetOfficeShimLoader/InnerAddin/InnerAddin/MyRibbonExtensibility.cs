bfad835caea4ade89cebcecc179269f13af0e894
SebastianDotNet <microschuft@hotmail.com>
2018-06-29 18:37:40 +0000

    - add missing MyRibbonExtensibility.cs to source control
    Imported from NetOffice r160
    https://osdn.net/projects/netoffice/scm/svn/commits/160

---
+++ NetOfficeShimLoader/InnerAddin/InnerAddin/MyRibbonExtensibility.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace InnerAddin
+{
+    internal class MyRibbonExtensibility : NetOffice.OfficeApi.Native.IRibbonExtensibility
+    {
+        public string GetCustomUI(string RibbonID)
+        {
+            MessageBox.Show("getCustomUI");
+            var result = ReadString("RibbonUI.xml", typeof(MyRibbonExtensibility).Assembly);
+            return result;
+        }
+
+        public string ReadString(string resourceAddress, Assembly assembly)
+        {
+            System.IO.Stream resourceStream = ReadStream(resourceAddress, assembly);
+            System.IO.StreamReader textStreamReader = new System.IO.StreamReader(resourceStream);
+            if (textStreamReader == null)
+                throw (new System.IO.IOException("Error accessing resource string."));
+
+            string text = textStreamReader.ReadToEnd();
+            textStreamReader.Close();
+            resourceStream.Close();
+            return text;
+        }
+
+        public Stream ReadStream(string resourceAddress, Assembly assembly)
+        {
+            System.IO.Stream resourceStream = assembly.GetManifestResourceStream(resourceAddress);
+            if (resourceStream == null)
+            {
+                string target = typeof(MyRibbonExtensibility).Namespace + "." + resourceAddress;
+                MessageBox.Show(target);
+                resourceStream = assembly.GetManifestResourceStream(target);
+            }
+
+            if (resourceStream == null)
+                throw (new System.IO.IOException("Error accessing resource Stream."));
+
+            return resourceStream;
+        }
+    }
+}
