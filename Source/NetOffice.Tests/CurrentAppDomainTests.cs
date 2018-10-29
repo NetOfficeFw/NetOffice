6bea2c782849707aec5980c12e55ea4625afcb46
Jozef Izso <jozef.izso@gmail.com>
2018-10-29 17:22:35 +0100

    Add unit tests for CurrentDomain_AssemblyResolve handler

---
+++ Source/NetOffice.Tests/CurrentAppDomainTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NetOffice.Tests
+{
+    public class CurrentAppDomainTests
+    {
+        [Test]
+        public void CurrentDomain_AssemblyResolve_TypeNameWithoutComma_DoesNotThrowException()
+        {
+            // Arange
+            var core = new Core();
+            core.Console.Mode = DebugConsoleMode.MemoryList;
+
+            // Act - will trigger CurrentDomain_AssemblyResolve handler
+            var unityApplicationClass = Type.GetType("UnityEngine.Application, UnityEngine");
+
+            // Assert
+            Assert.AreEqual(0, core.Console.Messages.Length);
+        }
+    }
+}
