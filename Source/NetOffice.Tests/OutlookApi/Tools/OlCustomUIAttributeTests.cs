d737ff6f2994ccfd296a485af549dfb8905360a5
Jozef Izso <jozef.izso@gmail.com>
2018-10-29 17:22:35 +0100

    Add regression test for OlRibbonType.Microsoft_Outlook_Mail_Compose value #223

---
+++ Source/NetOffice.Tests/OutlookApi/Tools/OlCustomUIAttributeTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using NetOffice.OutlookApi.Tools;
+
+namespace NetOffice.Tests.OutlookApi.Tools
+{
+    [TestFixture]
+    public class OlCustomUIAttributeTests
+    {
+        /// <summary>
+        /// Regression test for #223 (OlRibbonType.cs wrong enum for Microsoft.Outlook.Mail.Compose)
+        /// </summary>
+        [Test]
+        public void OlRibbonType_MailComposeValue_IsCorrectlyConvertedToRibbonId()
+        {
+            // Arrange
+            var attribute = new OlCustomUIAttribute(OlRibbonType.Microsoft_Outlook_Mail_Compose, "DummyValue.xml");
+
+            // Act
+            var actualRibbonId = attribute.RibbonID;
+
+            // Assert
+            Assert.AreEqual("Microsoft.Outlook.Mail.Compose", actualRibbonId);
+        }
+    }
+}
