27ba18e44f02fad7b442756e5c89ebe6436f5c35
Jozef Izso <jozef.izso@gmail.com>
2019-03-29 14:09:03 +0100

    NetOffice Developer Toolbox v1.7.3.1

--- Toolbox/Toolbox/Properties/AssemblyInfo.cs
+++ Toolbox/Toolbox/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NetOffice DeveloperToolbox")]
-[assembly: AssemblyDescription("some stuff in a box")]
+[assembly: AssemblyTitle("NetOffice Developer Toolbox")]
+[assembly: AssemblyDescription("Developer Toolbox for NetOffice solutions.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NetOffice")]
-[assembly: AssemblyCopyright("Copyright ©  2011 Sebastian Lange")]
+[assembly: AssemblyCopyright("Copyright © 2011-2015 Sebastian Lange, 2019 Jozef Izso")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.2.1")]
-[assembly: AssemblyFileVersion("1.4.2.1")]
+[assembly: AssemblyVersion("1.4.3.1")]
+[assembly: AssemblyFileVersion("1.4.3.1")]
