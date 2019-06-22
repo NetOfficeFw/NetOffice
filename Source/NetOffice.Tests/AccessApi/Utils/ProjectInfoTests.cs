b285adfc277ec81b8043999964b465466c52415b
Jozef Izso <jozef.izso@gmail.com>
2019-06-22 22:37:12 +0200

    Use correct value in ProjectInfo.Name of AccessApi library
    Fixes #231

---
+++ Source/NetOffice.Tests/AccessApi/Utils/ProjectInfoTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using AccessApi.Utils;
+
+namespace NetOffice.Tests.AccessApi.Utils
+{
+    [TestFixture]
+    public class ProjectInfoTests
+    {
+        /// <summary>
+        /// Regression test for #231 (Access library ProjectInfo returns incorrect AssemblyName value)
+        /// </summary>
+        [Test]
+        public void AssemblyName_HasCorrectValue()
+        {
+            // Arrange
+            var projectInfo = new ProjectInfo();
+
+            // Act
+            var actualAssemblyName = projectInfo.AssemblyName;
+
+            // Assert
+            Assert.AreEqual("AccessApi", actualAssemblyName);
+        }
+    }
+}
