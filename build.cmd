44966800c6fec0c214b40eaa2aa20773579d85c4
Jozef Izso <jozef.izso@gmail.com>
2019-06-22 23:31:40 +0200

    Add build script to prepare NetOffice package

---
+++ build.cmd
@@ -0,0 +1,27 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+set _version=1.7.4.6
+
+mkdir out
+
+xcopy /s /e /y Documentation out\Documentation\
+xcopy /s /e /y Examples out\Examples\
+xcopy /s /e /y Tutorials out\Tutorials\
+xcopy /s /e /y Source out\Source\
+
+nuget restore Source\NetOffice.sln
+msbuild Source\NetOffice.sln /t:Build /p:Configuration=Release /v:m
+
+del /s /q Source\ClientApplication\bin\Release\ClientApplication.*
+del /s /q Source\ClientApplication\bin\Release\stdole.dll
+
+xcopy /y Source\ClientApplication\bin\Release "out\Assemblies\Any CPU\"
+
+xcopy /y "Breaking Changes.txt" out\
+xcopy /y BugFixes.txt out\
+xcopy /y ChangeLog.txt out\
+
+pushd out
+7z a -tzip ..\NetOffice_v%_version%.zip .
+popd
