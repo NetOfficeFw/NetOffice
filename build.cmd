a97b900b902d94cc57c2506619b8e2e87ec69ce9
Jozef Izso <jozef.izso@gmail.com>
2020-03-08 22:56:39 +0100

    Update build script to create release artifact

--- build.cmd
+++ build.cmd
@@ -1,7 +1,10 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-set _version=1.8.0.0
+set _version=1.8.0
+set _suffix=alpha01
+::  goIT Solutions, s.r.o. certificate
+set _thumbprint=ac6dbffb1bf8b62281deb8641023a66cddc5db57
 
 mkdir out
 
@@ -10,18 +13,23 @@ xcopy /s /e /y Examples out\Examples\
 xcopy /s /e /y Tutorials out\Tutorials\
 xcopy /s /e /y Source out\Source\
 
-nuget restore Source\NetOffice.sln
-msbuild Source\NetOffice.sln /t:Build /p:Configuration=Release /v:m
+msbuild Source\NetOffice.sln /t:Restore;Build;Pack /p:IncludeSource=true /p:Configuration=Release /p:VersionSuffix="%_suffix%" /p:PackageOutputPath="%CD%\out" /v:m
+nuget.exe sign "out\*.nupkg" -CertificateFingerprint "%_thumbprint%" -HashAlgorithm SHA256 -Timestamper http://timestamp.comodoca.com -TimestampHashAlgorithm SHA256 -Overwrite -OutputDirectory out -NonInteractive -ForceEnglishOutput
 
 del /s /q Source\ClientApplication\bin\Release\ClientApplication.*
 del /s /q Source\ClientApplication\bin\Release\stdole.dll
 
 xcopy /y Source\ClientApplication\bin\Release "out\Assemblies\Any CPU\"
 
+rem del /s /q Source\ClientApplication\bin\Release\ClientApplication.*
+rem del /s /q Source\ClientApplication\bin\Release\*\stdole.dll
+
+xcopy /y /s /i /e Source\ClientApplication\bin\Release "out\Assemblies\"
+
 xcopy /y "Breaking Changes.txt" out\
 xcopy /y BugFixes.txt out\
 xcopy /y ChangeLog.txt out\
 
 pushd out
-7z a -tzip ..\NetOffice_v%_version%.zip .
+7z a -tzip ..\NetOffice_v%_version%-%_suffix%.zip .
 popd
