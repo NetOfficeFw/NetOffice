79e694f0503ae1905ab57c264b2c2a8bc4ea738e
Jozef Izso <jozef.izso@gmail.com>
2020-03-28 18:02:43 +0100

    Add tests for SlideShow related events in PowerPoint application #262

---
+++ Source/NetOffice.Tests/Helpers/ConnectionPointStub.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace NetOffice.Tests.Helpers
+{
+    public class ConnectionPointStub : IConnectionPoint
+    {
+        public void GetConnectionInterface(out Guid pIID)
+        {
+            pIID = Guid.Empty;
+        }
+
+        public void GetConnectionPointContainer(out IConnectionPointContainer ppCPC)
+        {
+            ppCPC = null;
+        }
+
+        public void Advise(object pUnkSink, out int pdwCookie)
+        {
+            pdwCookie = 0xCDCDCD;
+        }
+
+        public void Unadvise(int dwCookie)
+        {
+        }
+
+        public void EnumConnections(out IEnumConnections ppEnum)
+        {
+            ppEnum = null;
+        }
+    }
+}
---
+++ Source/NetOffice.Tests/Helpers/TestableComObjectStub.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using NetOffice.Availity;
+
+namespace NetOffice.Tests.Helpers
+{
+    internal class TestableComObjectStub : ICOMObject, IEventBinding
+    {
+        private List<ICOMObject> _children = new List<ICOMObject>();
+
+        public bool EventBridgeInitialized => throw new NotImplementedException();
+
+        public object SyncRoot => throw new NotImplementedException();
+
+        public Core Factory => Core.Default;
+
+        public Invoker Invoker => throw new NotImplementedException();
+
+        public Settings Settings => Core.Default.Settings;
+
+        public DebugConsole Console => throw new NotImplementedException();
+
+        public object UnderlyingObject => throw new NotImplementedException();
+
+        public Type UnderlyingType => throw new NotImplementedException();
+
+        public string UnderlyingTypeName => throw new NotImplementedException();
+
+        public string UnderlyingFriendlyTypeName => throw new NotImplementedException();
+
+        public string UnderlyingComponentName => throw new NotImplementedException();
+
+        public string InstanceName => throw new NotImplementedException();
+
+        public string InstanceFriendlyName => throw new NotImplementedException();
+
+        public string InstanceComponentName => throw new NotImplementedException();
+
+        public Type InstanceType => throw new NotImplementedException();
+
+        public bool IsDisposed => false;
+
+        public bool IsCurrentlyDisposing => false;
+
+        public ICOMObject ParentObject => throw new NotImplementedException();
+
+        public IEnumerable<ICOMObject> ChildObjects => _children;
+
+        public bool IsEventBinding => true;
+
+        public bool IsEventBridgeInitialized => true;
+
+        public bool IsWithEventRecipients => throw new NotImplementedException();
+
+        public event OnDisposeEventHandler OnDispose;
+
+        public void AddChildObject(ICOMObject childObject)
+        {
+            _children.Add(childObject);
+        }
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateEventBridge()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose(bool disposeEventBinding)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisposeChildInstances()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisposeChildInstances(bool disposeEventBinding)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisposeEventBridge()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EntityIsAvailable(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EntityIsAvailable(string name, SupportedEntityType searchType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetCountOfEventRecipients(string eventName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Delegate[] GetEventRecipients(string eventName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasEventRecipients()
+        {
+            return true;
+        }
+
+        public bool HasEventRecipients(string eventName)
+        {
+            return true;
+        }
+
+        public int RaiseCustomEvent(string eventName, ref object[] paramsArray)
+        {
+            this.LastRaisedEventName = eventName;
+            this.LastRaisedEventParameters = paramsArray;
+
+            return 0;
+        }
+
+        public bool RemoveChildObject(ICOMObject childObject)
+        {
+            return _children.Remove(childObject);
+        }
+
+        T ICOMObject.To<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string LastRaisedEventName { get; private set; }
+
+        public object[] LastRaisedEventParameters { get; private set; }
+    }
+}
