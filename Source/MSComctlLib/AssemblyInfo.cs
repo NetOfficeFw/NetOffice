1930c0f580877df5261cf1065c3e8f956063e4ba
Jozef Izso <jozef.izso@gmail.com>
2020-06-29 12:15:19 +0200

    NetOffice release v1.7.4.11

--- Source/MSComctlLib/AssemblyInfo.cs
+++ Source/MSComctlLib/AssemblyInfo.cs
@@ -19,12 +19,12 @@ using NetOffice.Attributes;
 [assembly: AssemblyCopyright("Copyright © 2012-2018 Sebastian Lange, © 2015-2020 Jozef Izso")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.7.4.10")]
-[assembly: AssemblyFileVersion("1.7.4.10")]
+[assembly: AssemblyVersion("1.7.4.11")]
+[assembly: AssemblyFileVersion("1.7.4.11")]
 [assembly: PrimaryInteropAssembly(1, 0)]
 [assembly: ImportedFromTypeLib("MSComctlLib")]
 [assembly: Guid("831FDD16-0C5C-11D2-A9FC-0000F8754DA1")]
-[assembly: NetOfficeAssemblyAttribute("1.7.4.10")]
+[assembly: NetOfficeAssemblyAttribute("1.7.4.11")]
 [assembly: Dependency("NetOffice.dll", LoadHint.Default)]
 
 
