862133a89c427174bb5c79c6d951997a07ae1333
Jozef Izso <jozef.izso@gmail.com>
2020-07-22 17:07:29 +0200

    Add unit and integration tests for CoClassEventReflector.HasEventRecipients() method #277

---
+++ Source/NetOffice.Tests/NetOffice/Events/CoClassEventReflectorTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using NetOffice.Events;
+
+namespace NetOffice.Tests.NetOffice.Events
+{
+    [TestFixture]
+    public class CoClassEventReflectorTests
+    {
+        [Test]
+        public void HasEventRecipients_NoSubscriber_ReturnsFalse()
+        {
+            // Arrange
+            var stub = new EventClassStub();
+
+            // Act
+            var actualValue = CoClassEventReflector.HasEventRecipients(stub, typeof(EventClassStub));
+
+            // Assert
+            Assert.IsFalse(actualValue);
+        }
+
+        [Test]
+        public void HasEventRecipients_EventNameParam_NoSubscriber_ReturnsFalse()
+        {
+            // Arrange
+            var stub = new EventClassStub();
+
+            // Act
+            var actualValue = CoClassEventReflector.HasEventRecipients(stub, typeof(EventClassStub), "Custom1");
+
+            // Assert
+            Assert.IsFalse(actualValue);
+        }
+
+        [Test]
+        public void HasEventRecipients_EventNameParam_NoSubscriberForNamedEvent_ReturnsFalse()
+        {
+            // Arrange
+            var stub = new EventClassStub();
+            stub.Custom1Event += (obj, arg) => { };
+
+            // Act
+            var actualValue = CoClassEventReflector.HasEventRecipients(stub, typeof(EventClassStub), "Custom2");
+
+            // Assert
+            Assert.IsFalse(actualValue);
+        }
+
+        [Test]
+        public void HasEventRecipients_EventWithSubscriber_ReturnsTrue()
+        {
+            // Arrange
+            var stub = new EventClassStub();
+            stub.Custom1Event += (sender, args) => { };
+
+            // Act
+            var actualValue = CoClassEventReflector.HasEventRecipients(stub, typeof(EventClassStub));
+
+            // Assert
+            Assert.IsTrue(actualValue);
+        }
+
+        [Test]
+        public void HasEventRecipients_EventNameParam_EventWithSubscriber_ReturnsTrue()
+        {
+            // Arrange
+            var stub = new EventClassStub();
+            stub.Custom1Event += (sender, args) => { };
+
+            // Act
+            var actualValue = CoClassEventReflector.HasEventRecipients(stub, typeof(EventClassStub), "Custom1");
+
+            // Assert
+            Assert.IsTrue(actualValue);
+        }
+    }
+}
---
+++ Source/NetOffice.Tests/NetOffice/Events/EventClassStub.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NetOffice.Tests.Helpers;
+
+namespace NetOffice.Tests.NetOffice.Events
+{
+    internal class EventClassStub : TestableComObjectStub
+    {
+        // backing fields for events in NetOffice must start with _ and end with 'Event'
+        private event EventHandler _Custom1Event;
+        private event EventHandler _Custom2Event;
+        private event EventHandler _Custom3Event;
+
+        public event EventHandler Custom1Event
+        {
+            add => _Custom1Event += value;
+            remove => _Custom1Event -= value;
+        }
+
+        public event EventHandler Custom2Event
+        {
+            add => _Custom2Event += value;
+            remove => _Custom2Event -= value;
+        }
+
+        public event EventHandler Custom3Event
+        {
+            add => _Custom3Event += value;
+            remove => _Custom3Event -= value;
+        }
+    }
+}
