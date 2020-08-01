16be8c91cd7a2c839d06356a42afccce6c1a80a9
Jozef Izso <jozef.izso@gmail.com>
2020-08-01 22:02:58 +0200

    Add test for ProjectInfo.AssemlbyName in Outlook library

---
+++ Source/NetOffice.Tests/OutlookApi/Utils/ProjectInfoTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using OutlookApi.Utils;
+
+namespace NetOffice.Tests.OutlookApi.Utils
+{
+    [TestFixture]
+    public class ProjectInfoTests
+    {
+        [Test]
+        public void AssemblyName_HasCorrectValue()
+        {
+            // Arrange
+            var projectInfo = new ProjectInfo();
+
+            // Act
+            var actualAssemblyName = projectInfo.AssemblyName;
+
+            // Assert
+            Assert.AreEqual("OutlookApi", actualAssemblyName);
+        }
+    }
+}
