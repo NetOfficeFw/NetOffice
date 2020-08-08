4d39da6a20f58f51a71cc95562acb65ef282e811
Jozef Izso <jozef.izso@gmail.com>
2020-08-08 23:21:06 +0200

    Use type references in doc comments

--- Source/NetOffice/Extensions/ConversionExtensions.cs
+++ Source/NetOffice/Extensions/ConversionExtensions.cs
@@ -5,18 +5,18 @@ using NetOffice.Exceptions;
 namespace NetOffice.Extensions.Conversion
 {
     /// <summary>
-    /// ICOMObject Conversion Extensions
+    /// <see cref="ICOMObject"/> Conversion Extensions
     /// </summary>
     public static class ConversionExtensions
     {
         /// <summary>
-        /// Cast instance to ICOMObject and clone instance as target type of T
+        /// Cast instance to <see cref="ICOMObject"/> and clone instance as target type of T
         /// </summary>
         /// <typeparam name="T">given target type</typeparam>
         /// <param name="value">instance to convert</param>
         /// <param name="throwException">return null or throw exception if its failed to convert</param>
         /// <returns>instance of T or null(Nothing in Visual Basic)</returns>
-        /// <exception cref="CloneException">Failed to convert instance to ICOMObject</exception>
+        /// <exception cref="CloneException">Failed to convert instance to <see cref="ICOMObject"/></exception>
         public static T To<T>(this object value, bool throwException = false) where T:class, ICOMObject
         {
             ICOMObject comObject = value as ICOMObject;
