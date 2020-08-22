2cad479e1bb7f2abd174b89e187dd9def1965067
Jozef Izso <jozef.izso@gmail.com>
2020-08-22 15:50:37 +0200

    Improve InvokerCOMException class design

--- Source/NetOffice/Exceptions/InvokerCOMException.cs
+++ Source/NetOffice/Exceptions/InvokerCOMException.cs
@@ -3,27 +3,25 @@
 namespace NetOffice.Exceptions
 {
     /// <summary>
-    /// Indicates a failed invoke operation
+    /// Indicates a failed invoke operation.
     /// </summary>
     public abstract class InvokerCOMException : NetOfficeCOMException
     {
         /// <summary>
-        /// Creates an instance of the class
+        /// Creates an instance of the InvokerCOMException class.
         /// </summary>
         /// <param name="message">the message that indicates the reason for the exception</param>
         /// <param name="innerException">inner exception</param>
-        public InvokerCOMException(string message, Exception innerException) : base(message, innerException)
+        protected InvokerCOMException(string message, Exception innerException) : base(message, innerException)
         {
-
         }
 
         /// <summary>
-        /// Creates an instance of the class
+        /// Creates an instance of the InvokerCOMException class.
         /// </summary>
         /// <param name="innerException">inner exception</param>
-        public InvokerCOMException(Exception innerException) : base(null != innerException ? innerException.Message : "Failed to invoke property.", innerException)
+        protected InvokerCOMException(Exception innerException) : base(innerException?.Message ?? "Failed to invoke property.", innerException)
         {
-
         }
     }
 }
