4e6b4007f1760e0ccf3bd351952fdd11e0528e18
Jozef Izso <jozef.izso@gmail.com>
2020-08-22 16:35:02 +0200

    Add unit tests for Settings.IsEqualTo when EnableSafeMode is different #291

---
+++ Source/NetOffice.Tests/NetOffice/SettingsTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NetOffice.Tests.NetOffice
+{
+    [TestFixture]
+    public class SettingsTests
+    {
+        [Test]
+        public void IsEqualTo_DefaultContructor_ReturnsTrue()
+        {
+            // Arrange
+            var settings1 = new Settings();
+            var settings2 = new Settings();
+
+            // Act
+            var actualResult = settings1.IsEqualTo(settings2);
+
+            // Assert
+            Assert.IsTrue(actualResult);
+        }
+
+        [Test]
+        public void IsEqualTo_EnableSafeModeValueIsDifferent_ReturnsFalse()
+        {
+            // Arrange
+            var settings1 = new Settings();
+            var settings2 = new Settings();
+
+            settings1.EnableSafeMode = false;
+            settings1.EnableSafeMode = true;
+
+            // Act
+            var actualResult = settings1.IsEqualTo(settings2);
+
+            // Assert
+            Assert.IsFalse(actualResult, "Settings objects must not be equal when EnableSafeMode value is different.");
+        }
+    }
+}
