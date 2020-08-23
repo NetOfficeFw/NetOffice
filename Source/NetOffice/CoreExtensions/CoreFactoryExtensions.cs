87f315516aaf5dc433d827d86cff6e1d9332bc0c
Jozef Izso <jozef.izso@gmail.com>
2020-08-23 14:05:57 +0200

    Remove redundant catch clause

--- Source/NetOffice/CoreExtensions/CoreFactoryExtensions.cs
+++ Source/NetOffice/CoreExtensions/CoreFactoryExtensions.cs
@@ -244,10 +244,6 @@ namespace NetOffice
             {
                 throw new IDispatchNotImplementedException(exception);
             }
-            catch
-            {
-                throw;
-            }
         }
     }
 }
