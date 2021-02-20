c71637764ca2973308eabc758c2308a74fbf95d4
Jozef Izso <jozef.izso@gmail.com>
2021-02-20 14:13:41 +0100

    Parser for the resiliency binary data stored by Microsoft Office when add-ins crash.

---
+++ Source/NetOffice/Tools/DisabledItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NetOffice.Tools
+{
+    /// <summary>
+    /// Represents information about a disabled add-in in a Microsoft Office application.
+    /// </summary>
+    /// <remarks>
+    /// Use the <see cref="OfficeResiliency.Parse()"/> method to convert the binary
+    /// data from the Resiliency\DisabledItems registry keys into the <see cref="DisabledItem"/> object.
+    /// </remarks>
+    public class DisabledItem
+    {
+        public DisabledItemType DisabledItemType { get; set; }
+
+        public string FriendlyName { get; set; }
+
+        public string Module { get; set; }
+    }
+}
