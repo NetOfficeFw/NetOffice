4f25c2dbf60c42763f98c2cb4ca563a16f062bab
Steven Lovelock <70571880+elan-slovelock@users.noreply.github.com>
2023-11-16 15:31:49 +0000

    Add Workbook.Queries

---
+++ Source/Excel/DispatchInterfaces/Queries.cs
@@ -0,0 +1,275 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using NetRuntimeSystem = System;
+using System.ComponentModel;
+using NetOffice.Attributes;
+using NetOffice.CollectionsGeneric;
+
+namespace NetOffice.ExcelApi
+{
+	/// <summary>
+	/// DispatchInterface Queries 
+	/// SupportByVersion Excel, 16
+	/// </summary>
+	/// <remarks> Docs: <see href="https://docs.microsoft.com/en-us/office/vba/api/Excel.Queries"/> </remarks>
+	[SupportByVersion("Excel", 16)]
+	[EntityType(EntityType.IsDispatchInterface), Enumerator(Enumerator.Reference, EnumeratorInvoke.Property), HasIndexProperty(IndexInvoke.Property, "_Default")]
+	public class Queries : COMObject, IEnumerableProvider<NetOffice.ExcelApi.WorkbookQuery>
+	{
+		#pragma warning disable
+
+		#region Type Information
+
+		/// <summary>
+		/// Instance Type
+		/// </summary>
+		[EditorBrowsable(EditorBrowsableState.Advanced), Browsable(false), Category("NetOffice"), CoreOverridden]
+		public override Type InstanceType
+		{
+			get
+			{
+				return LateBindingApiWrapperType;
+			}
+		}
+
+		private static Type _type;
+
+		[EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
+		public static Type LateBindingApiWrapperType
+		{
+			get
+			{
+				if (null == _type)
+					_type = typeof(Queries);
+				return _type;
+			}
+		}
+		
+		#endregion
+		
+		#region Ctor
+
+		/// <param name="factory">current used factory core</param>
+		/// <param name="parentObject">object there has created the proxy</param>
+		/// <param name="proxyShare">proxy share instead if com proxy</param>
+		public Queries(Core factory, ICOMObject parentObject, COMProxyShare proxyShare) : base(factory, parentObject, proxyShare)
+		{
+		}
+
+		///<param name="factory">current used factory core</param>
+		///<param name="parentObject">object there has created the proxy</param>
+		///<param name="comProxy">inner wrapped COM proxy</param>
+		public Queries(Core factory, ICOMObject parentObject, object comProxy) : base(factory, parentObject, comProxy)
+		{
+			
+		}
+
+		///<param name="parentObject">object there has created the proxy</param>
+		///<param name="comProxy">inner wrapped COM proxy</param>
+		[EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
+		public Queries(ICOMObject parentObject, object comProxy) : base(parentObject, comProxy)
+		{
+		}
+		
+		///<param name="factory">current used factory core</param>
+		///<param name="parentObject">object there has created the proxy</param>
+		///<param name="comProxy">inner wrapped COM proxy</param>
+		///<param name="comProxyType">Type of inner wrapped COM proxy"</param>
+		[EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
+		public Queries(Core factory, ICOMObject parentObject, object comProxy, NetRuntimeSystem.Type comProxyType) : base(factory, parentObject, comProxy, comProxyType)
+		{
+
+		}
+
+		///<param name="parentObject">object there has created the proxy</param>
+		///<param name="comProxy">inner wrapped COM proxy</param>
+		///<param name="comProxyType">Type of inner wrapped COM proxy"</param>
+		[EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
+		public Queries(ICOMObject parentObject, object comProxy, NetRuntimeSystem.Type comProxyType) : base(parentObject, comProxy, comProxyType)
+		{
+		}
+		
+		///<param name="replacedObject">object to replaced. replacedObject are not usable after this action</param>
+		[EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
+		public Queries(ICOMObject replacedObject) : base(replacedObject)
+		{
+		}
+		
+		[EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
+		public Queries() : base()
+		{
+		}
+		
+		/// <param name="progId">registered progID</param>
+		[EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
+		public Queries(string progId) : base(progId)
+		{
+		}
+		
+		#endregion
+		
+		#region Properties
+
+		/// <summary>
+		/// SupportByVersion Excel 16
+		/// Get
+		/// </summary>
+		/// <remarks> Docs: <see href="https://docs.microsoft.com/en-us/office/vba/api/Excel.Queries.Application"/> </remarks>
+		[SupportByVersion("Excel", 16)]
+		public NetOffice.ExcelApi.Application Application
+		{
+			get
+			{
+				return Factory.ExecuteKnownReferencePropertyGet<NetOffice.ExcelApi.Application>(this, "Application", NetOffice.ExcelApi.Application.LateBindingApiWrapperType);
+			}
+		}
+
+		/// <summary>
+		/// SupportByVersion Excel 16
+		/// Get
+		/// </summary>
+		/// <remarks> Docs: <see href="https://docs.microsoft.com/en-us/office/vba/api/Excel.Queries.Creator"/> </remarks>
+		[SupportByVersion("Excel", 16)]
+		public NetOffice.ExcelApi.Enums.XlCreator Creator
+		{
+			get
+			{
+				return Factory.ExecuteEnumPropertyGet<NetOffice.ExcelApi.Enums.XlCreator>(this, "Creator");
+			}
+		}
+
+		/// <summary>
+		/// SupportByVersion Excel 16
+		/// Get
+		/// Unknown COM Proxy
+		/// </summary>
+		/// <remarks> Docs: <see href="https://docs.microsoft.com/en-us/office/vba/api/Excel.Queries.Parent"/> </remarks>
+		[SupportByVersion("Excel", 16), ProxyResult]
+		public object Parent
+		{
+			get
+			{
+				return Factory.ExecuteReferencePropertyGet(this, "Parent");
+			}
+		}
+
+		/// <summary>
+		/// SupportByVersion Excel 16
+		/// Get
+		/// </summary>
+		/// <param name="index">object index</param>
+		[SupportByVersion("Excel", 16)]
+		[NetRuntimeSystem.Runtime.CompilerServices.IndexerName("Item"), IndexProperty]
+		public NetOffice.ExcelApi.WorkbookQuery this[object index]
+		{
+			get
+			{
+				return Factory.ExecuteKnownReferencePropertyGet<NetOffice.ExcelApi.WorkbookQuery>(this, "_Default", NetOffice.ExcelApi.WorkbookQuery.LateBindingApiWrapperType, index);
+			}
+		}
+
+		/// <summary>
+		/// SupportByVersion Excel 16
+		/// Get
+		/// </summary>
+		/// <remarks> Docs: <see href="https://docs.microsoft.com/en-us/office/vba/api/Excel.Queries.Count"/> </remarks>
+		[SupportByVersion("Excel", 16)]
+		public Int32 Count
+		{
+			get
+			{
+				return Factory.ExecuteInt32PropertyGet(this, "Count");
+			}
+		}
+
+		/// <summary>
+		/// SupportByVersion Excel 16
+		/// Get
+		/// </summary>
+		/// <remarks> Docs: <see href="https://docs.microsoft.com/en-us/office/vba/api/Excel.Queries.FastCombine"/> </remarks>
+		[SupportByVersion("Excel", 16)]
+		public bool FastCombine
+		{
+			get
+			{
+				return Factory.ExecuteBoolPropertyGet(this, "FastCombine");
+			}
+		}
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// SupportByVersion Excel 16
+		/// </summary>
+		/// <remarks> Docs: <see href="https://docs.microsoft.com/en-us/office/vba/api/Excel.Queries.Add"/> </remarks>
+		/// <param name="name">name of the query</param>
+		/// <param name="formula">Power Query M formula for the new query</param>
+		/// <param name="description">optional description of the query</param>
+		[SupportByVersion("Excel", 16)]
+		public NetOffice.ExcelApi.WorkbookQuery Add(string name, string formula, object description)
+		{
+			return Factory.ExecuteKnownReferenceMethodGet<NetOffice.ExcelApi.WorkbookQuery>(this, "Add", NetOffice.ExcelApi.WorkbookQuery.LateBindingApiWrapperType, name, formula, description);
+		}
+
+		/// <summary>
+		/// SupportByVersion Excel 16
+		/// </summary>
+		/// <remarks> Docs: <see href="https://docs.microsoft.com/en-us/office/vba/api/Excel.Queries.Add"/> </remarks>
+		/// <param name="name">name of the query</param>
+		/// <param name="formula">Power Query M formula for the new query</param>
+		[SupportByVersion("Excel", 16)]
+		public NetOffice.ExcelApi.WorkbookQuery Add(string name, string formula)
+		{
+			return Factory.ExecuteKnownReferenceMethodGet<NetOffice.ExcelApi.WorkbookQuery>(this, "Add", NetOffice.ExcelApi.WorkbookQuery.LateBindingApiWrapperType, name, formula);
+		}
+
+		#endregion
+
+		#region IEnumerableProvider<NetOffice.ExcelApi.WorkbookQuery>
+
+		ICOMObject IEnumerableProvider<NetOffice.ExcelApi.WorkbookQuery>.GetComObjectEnumerator(ICOMObject parent)
+		{
+			return NetOffice.Utils.GetComObjectEnumeratorAsProperty(parent, this);
+		}
+
+		IEnumerable IEnumerableProvider<NetOffice.ExcelApi.WorkbookQuery>.FetchVariantComObjectEnumerator(ICOMObject parent, ICOMObject enumerator)
+		{
+			return NetOffice.Utils.FetchVariantComObjectEnumerator(parent, enumerator, false);
+		}
+
+		#endregion
+
+		#region IEnumerable<NetOffice.ExcelApi.WorkbookQuery>
+
+		/// <summary>
+		/// SupportByVersion Excel, 16
+		/// </summary>
+		[SupportByVersion("Excel", 16)]
+		public IEnumerator<NetOffice.ExcelApi.WorkbookQuery> GetEnumerator()
+		{
+			NetRuntimeSystem.Collections.IEnumerable innerEnumerator = (this as NetRuntimeSystem.Collections.IEnumerable);
+			foreach (NetOffice.ExcelApi.WorkbookQuery item in innerEnumerator)
+				yield return item;
+		}
+
+		#endregion
+
+		#region IEnumerable
+
+		/// <summary>
+		/// SupportByVersion Excel, 16
+		/// </summary>
+		[SupportByVersion("Excel", 16)]
+		IEnumerator NetRuntimeSystem.Collections.IEnumerable.GetEnumerator()
+		{
+			return NetOffice.Utils.GetProxyEnumeratorAsProperty(this);
+		}
+
+		#endregion
+
+		#pragma warning restore
+	}
+}
---
+++ Source/Excel/DispatchInterfaces/WorkbookQuery.cs
@@ -0,0 +1,240 @@
+﻿using System.Reflection;
+using System;
+using NetRuntimeSystem = System;
+using System.ComponentModel;
+using NetOffice.Attributes;
+
+namespace NetOffice.ExcelApi
+{
+	/// <summary>
+	/// DispatchInterface WorkbookQuery 
+	/// SupportByVersion Excel, 16
+	/// </summary>
+	/// <remarks> Docs: <see href="https://docs.microsoft.com/en-us/office/vba/api/Excel.WorkbookQuery"/> </remarks>
+	[SupportByVersion("Excel", 16)]
+	[EntityType(EntityType.IsDispatchInterface)]
+ 	public class WorkbookQuery : COMObject
+	{
+		#pragma warning disable
+
+		#region Type Information
+
+		/// <summary>
+		/// Instance Type
+		/// </summary>
+		[EditorBrowsable(EditorBrowsableState.Advanced), Browsable(false), Category("NetOffice"), CoreOverridden]
+		public override Type InstanceType
+		{
+			get
+			{
+				return LateBindingApiWrapperType;
+			}
+		}
+
+		private static Type _type;
+
+		[EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
+		public static Type LateBindingApiWrapperType
+		{
+			get
+			{
+				if (null == _type)
+					_type = typeof(WorkbookQuery);
+				return _type;
+			}
+		}
+		
+		#endregion
+		
+		#region Ctor
+
+		/// <param name="factory">current used factory core</param>
+		/// <param name="parentObject">object there has created the proxy</param>
+		/// <param name="proxyShare">proxy share instead if com proxy</param>
+		public WorkbookQuery(Core factory, ICOMObject parentObject, COMProxyShare proxyShare) : base(factory, parentObject, proxyShare)
+		{
+		}
+
+		///<param name="factory">current used factory core</param>
+		///<param name="parentObject">object there has created the proxy</param>
+		///<param name="comProxy">inner wrapped COM proxy</param>
+		public WorkbookQuery(Core factory, ICOMObject parentObject, object comProxy) : base(factory, parentObject, comProxy)
+		{
+			
+		}
+
+		///<param name="parentObject">object there has created the proxy</param>
+		///<param name="comProxy">inner wrapped COM proxy</param>
+		[EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
+		public WorkbookQuery(ICOMObject parentObject, object comProxy) : base(parentObject, comProxy)
+		{
+		}
+		
+		///<param name="factory">current used factory core</param>
+		///<param name="parentObject">object there has created the proxy</param>
+		///<param name="comProxy">inner wrapped COM proxy</param>
+		///<param name="comProxyType">Type of inner wrapped COM proxy"</param>
+		[EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
+		public WorkbookQuery(Core factory, ICOMObject parentObject, object comProxy, NetRuntimeSystem.Type comProxyType) : base(factory, parentObject, comProxy, comProxyType)
+		{
+
+		}
+
+		///<param name="parentObject">object there has created the proxy</param>
+		///<param name="comProxy">inner wrapped COM proxy</param>
+		///<param name="comProxyType">Type of inner wrapped COM proxy"</param>
+		[EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
+		public WorkbookQuery(ICOMObject parentObject, object comProxy, NetRuntimeSystem.Type comProxyType) : base(parentObject, comProxy, comProxyType)
+		{
+		}
+		
+		///<param name="replacedObject">object to replaced. replacedObject are not usable after this action</param>
+		[EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
+		public WorkbookQuery(ICOMObject replacedObject) : base(replacedObject)
+		{
+		}
+		
+		[EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
+		public WorkbookQuery() : base()
+		{
+		}
+		
+		/// <param name="progId">registered progID</param>
+		[EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
+		public WorkbookQuery(string progId) : base(progId)
+		{
+		}
+		
+		#endregion
+		
+		#region Properties
+
+		/// <summary>
+		/// SupportByVersion Excel 16
+		/// Get
+		/// </summary>
+		/// <remarks> Docs: <see href="https://docs.microsoft.com/en-us/office/vba/api/Excel.WorkbookQuery.Application"/> </remarks>
+		[SupportByVersion("Excel", 16)]
+		public NetOffice.ExcelApi.Application Application
+		{
+			get
+			{
+				return Factory.ExecuteKnownReferencePropertyGet<NetOffice.ExcelApi.Application>(this, "Application", NetOffice.ExcelApi.Application.LateBindingApiWrapperType);
+			}
+		}
+
+		/// <summary>
+		/// SupportByVersion Excel 16
+		/// Get
+		/// </summary>
+		/// <remarks> Docs: <see href="https://docs.microsoft.com/en-us/office/vba/api/Excel.WorkbookQuery.Creator"/> </remarks>
+		[SupportByVersion("Excel", 16)]
+		public NetOffice.ExcelApi.Enums.XlCreator Creator
+		{
+			get
+			{
+				return Factory.ExecuteEnumPropertyGet<NetOffice.ExcelApi.Enums.XlCreator>(this, "Creator");
+			}
+		}
+
+		/// <summary>
+		/// SupportByVersion Excel 16
+		/// Get
+		/// Unknown COM Proxy
+		/// </summary>
+		/// <remarks> Docs: <see href="https://docs.microsoft.com/en-us/office/vba/api/Excel.WorkbookQuery.Parent"/> </remarks>
+		[SupportByVersion("Excel", 16), ProxyResult]
+		public object Parent
+		{
+			get
+			{
+				return Factory.ExecuteReferencePropertyGet(this, "Parent");
+			}
+		}
+
+		/// <summary>
+		/// SupportByVersion Excel 16
+		/// Get
+		/// </summary>
+		[SupportByVersion("Excel", 16)]
+		public string _Default
+		{
+			get
+			{
+				return Factory.ExecuteStringPropertyGet(this, "_Default");
+			}
+		}
+
+		/// <summary>
+		/// SupportByVersion Excel 16
+		/// Get/Set
+		/// </summary>
+		/// <remarks> Docs: <see href="https://docs.microsoft.com/en-us/office/vba/api/Excel.WorkbookQuery.Name"/> </remarks>
+		[SupportByVersion("Excel", 16)]
+		public string Name
+		{
+			get
+			{
+				return Factory.ExecuteStringPropertyGet(this, "Name");
+			}
+			set
+			{
+				Factory.ExecuteValuePropertySet(this, "Name", value);
+			}
+		}
+
+		/// <summary>
+		/// SupportByVersion Excel 16
+		/// Get/Set
+		/// </summary>
+		/// <remarks> Docs: <see href="https://docs.microsoft.com/en-us/office/vba/api/Excel.WorkbookQuery.Description"/> </remarks>
+		[SupportByVersion("Excel", 16)]
+		public string Description
+		{
+			get
+			{
+				return Factory.ExecuteStringPropertyGet(this, "Description");
+			}
+			set
+			{
+				Factory.ExecuteValuePropertySet(this, "Description", value);
+			}
+		}
+
+		/// <summary>
+		/// SupportByVersion Excel 16
+		/// Get/Set
+		/// </summary>
+		/// <remarks> Docs: <see href="https://docs.microsoft.com/en-us/office/vba/api/Excel.WorkbookQuery.Formula"/> </remarks>
+		[SupportByVersion("Excel", 16)]
+		public string Formula
+		{
+			get
+			{
+				return Factory.ExecuteStringPropertyGet(this, "Formula");
+			}
+			set
+			{
+				Factory.ExecuteValuePropertySet(this, "Formula", value);
+			}
+		}
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// SupportByVersion Excel 16
+		/// </summary>
+		/// <remarks> Docs: <see href="https://docs.microsoft.com/en-us/office/vba/api/Excel.WorkbookQuery.Delete"/> </remarks>
+		[SupportByVersion("Excel", 16)]
+		public void Delete()
+		{
+			 Factory.ExecuteMethod(this, "Delete");
+		}
+
+		#endregion
+
+		#pragma warning restore
+	}
+}
