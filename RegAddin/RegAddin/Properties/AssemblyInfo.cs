9c6a3bc53dfadfa966bbb6a4bbd3e4082253946a
Jozef Izso <jozef.izso@gmail.com>
2024-02-16 20:06:08 +0100

    Upgrade app to use modern SDK project format

--- RegAddin/RegAddin/Properties/AssemblyInfo.cs
+++ RegAddin/RegAddin/Properties/AssemblyInfo.cs
@@ -1,42 +1,8 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("RegAddin")]
-[assembly: AssemblyDescription("Register/Unregister MS-Office Addins")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("http://netoffice.codeplex.com")]
-[assembly: AssemblyProduct("RegAddin")]
-[assembly: AssemblyCopyright("Copyright ©  2016 Sebastian Lange")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo("RegAddinTest,PublicKey=" +
     "0024000004800000940000000602000000240000525341310004000001000100298c0fa7dc04d8" +
     "ce948de923ae689b2fb44d095d6f04e428bd8c56b4821a3bd772b96fb3f5bfbde1f2a48d085343" +
     "c0a2a00adb0ad29e3999a837ee0ca4487bae0280f48be08451214d20568f717a0fc0c392f589a4" +
     "bde589e10eebedcec6b013ae09b3879934f0639050558a19b36a68cb74e822dce602fc51918041" +
     "21ea45ce")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9c4a6abc-dd1a-47e7-87ea-ccd02529b327")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
