0c2e33a0294ce6f18694a4bb6ba080a5635325b2
Jozef Izso <jozef.izso@gmail.com>
2025-11-02 22:21:29 +0100

    Add integration test for presentation saved in OneDrive and with Auto Save feature enabled

--- Source/NetOffice.Tests/PowerPointApi/PresentationTests.cs
+++ Source/NetOffice.Tests/PowerPointApi/PresentationTests.cs
@@ -38,15 +38,38 @@ namespace NetOffice.Tests.PowerPointApi
 
             // Assert
             Assert.IsFalse(actualValue);
+
+            // Cleanup
+            presentation.Close();
+        }
+
+        [Test]
+        public void AutoSaveOn_PresentationSavedInOneDrive_FeatureAutoSaveIsOn_ReturnsTrue()
+        {
+            // Arrange
+            var presentation = PowerPointApp.Presentations.Open(this.Docs.AutoSaveOn_PresentationSavedInOneDrive_FeatureAutoSaveIsOn, false);
+
+            // Act
+            var actualValue = presentation.AutoSaveOn;
+
+            // Assert
+            Assert.IsTrue(actualValue);
+
+            // Cleanup
+            presentation.Close();
         }
 
         public class Documents
         {
             public string AutoSaveOn_PresentationSavedLocally_FeatureAutoSaveIsOff {  get; set; }
+            public string AutoSaveOn_PresentationSavedInOneDrive_FeatureAutoSaveIsOn { get; set; }
 
             public Documents(TestContext context)
             {
                 this.AutoSaveOn_PresentationSavedLocally_FeatureAutoSaveIsOff = Path.Combine(context.TestDirectory, @"PowerPointApi\Docs", "AutoSaveOn_PresentationSavedLocally_FeatureAutoSaveIsOff.pptx");
+
+                // Public link: https://1drv.ms/p/c/8cd14a64b99957bc/EVX1bUNmBtxDvfvg-aNQjKkBX35RJ3BJ6_2ey2Ox5gnIfA?e=DVyExX
+                this.AutoSaveOn_PresentationSavedInOneDrive_FeatureAutoSaveIsOn = @"https://d.docs.live.net/8CD14A64B99957BC/Developer/NetOfficeFw/PowerPoint/AutoSaveOn_PresentationSavedInOneDrive_FeatureAutoSaveIsOn.pptx";
             }
         }
     }
