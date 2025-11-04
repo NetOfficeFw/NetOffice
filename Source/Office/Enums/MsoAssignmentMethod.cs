60a3bdbe932a1736ce0f02f8b0d8e416c7507ea4
Jozef Izso <izso@cisco.com>
2025-11-04 22:08:30 +0100

    Add documentation comments to `MsoAssignmentMethod` enum

--- Source/Office/Enums/MsoAssignmentMethod.cs
+++ Source/Office/Enums/MsoAssignmentMethod.cs
@@ -11,32 +11,31 @@ using NetOffice.Attributes;
 namespace NetOffice.OfficeApi.Enums
 {
 	/// <summary>
-	/// SupportByVersion Office 16
+	/// Indicates the assignment method in a <see cref="LabelInfo"/> object.
 	/// </summary>
-	/// <remarks> 
-	/// Specifies the assignment method for a sensitivity label.
-	/// <para>MSDN Online Documentation: <see href="https://learn.microsoft.com/en-us/office/vba/api/office.msoassignmentmethod"/></para>
+	/// <remarks>
+	/// <para>MSDN Online Documentation: <see href="https://learn.microsoft.com/en-us/office/vba/api/overview/library-reference/msoassignmentmethod-enumeration-office"/></para>
 	/// </remarks>
 	[SupportByVersion("Office", 16)]
 	[EntityType(EntityType.IsEnum)]
 	public enum MsoAssignmentMethod
 	{
 		/// <summary>
-		/// SupportByVersion Office 16
+		/// The label is applied by default.
 		/// </summary>
 		/// <remarks>0</remarks>
 		[SupportByVersion("Office", 16)]
 		STANDARD = 0,
 
 		/// <summary>
-		/// SupportByVersion Office 16
+		/// The label was manually selected.
 		/// </summary>
 		/// <remarks>1</remarks>
 		[SupportByVersion("Office", 16)]
 		PRIVILEGED = 1,
 
 		/// <summary>
-		/// SupportByVersion Office 16
+		/// The label is applied automatically.
 		/// </summary>
 		/// <remarks>2</remarks>
 		[SupportByVersion("Office", 16)]
