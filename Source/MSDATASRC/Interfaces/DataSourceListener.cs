3e19e98ac36dc9bbc1b70267d1c12595ed3e71e6
Jozef Izso <jozef.izso@gmail.com>
2026-09-05 14:59:56 +0200

    Convert indentation from tabs to spaces in project `MSDATASRCApi.csproj`

--- Source/MSDATASRC/Interfaces/DataSourceListener.cs
+++ Source/MSDATASRC/Interfaces/DataSourceListener.cs
@@ -5,33 +5,33 @@ using NetOffice.Attributes;
 
 namespace NetOffice.MSDATASRCApi
 {
-	/// <summary>
-	/// Interface DataSourceListener 
-	/// SupportByVersion MSDATASRC, 4
-	/// </summary>
-	[SupportByVersion("MSDATASRC", 4)]
-	[EntityType(EntityType.IsInterface)]
- 	public class DataSourceListener : COMObject
-	{
-		#pragma warning disable
+    /// <summary>
+    /// Interface DataSourceListener 
+    /// SupportByVersion MSDATASRC, 4
+    /// </summary>
+    [SupportByVersion("MSDATASRC", 4)]
+    [EntityType(EntityType.IsInterface)]
+    public class DataSourceListener : COMObject
+    {
+        #pragma warning disable
 
-		#region Type Information
+        #region Type Information
 
-		/// <summary>
-		/// Instance Type
-		/// </summary>
-		[EditorBrowsable(EditorBrowsableState.Advanced), Browsable(false), Category("NetOffice"), CoreOverridden]
-		public override Type InstanceType
-		{
-			get
-			{
-				return LateBindingApiWrapperType;
-			}
-		}
+        /// <summary>
+        /// Instance Type
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Advanced), Browsable(false), Category("NetOffice"), CoreOverridden]
+        public override Type InstanceType
+        {
+            get
+            {
+                return LateBindingApiWrapperType;
+            }
+        }
 
         private static Type _type;
 
-		[EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
+        [EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
         public static Type LateBindingApiWrapperType
         {
             get
@@ -44,108 +44,108 @@ namespace NetOffice.MSDATASRCApi
         
         #endregion
         
-		#region Ctor
+        #region Ctor
 
-		/// <param name="factory">current used factory core</param>
-		/// <param name="parentObject">object there has created the proxy</param>
-		/// <param name="proxyShare">proxy share instead if com proxy</param>
-		public DataSourceListener(Core factory, ICOMObject parentObject, COMProxyShare proxyShare) : base(factory, parentObject, proxyShare)
-		{
-		}
+        /// <param name="factory">current used factory core</param>
+        /// <param name="parentObject">object there has created the proxy</param>
+        /// <param name="proxyShare">proxy share instead if com proxy</param>
+        public DataSourceListener(Core factory, ICOMObject parentObject, COMProxyShare proxyShare) : base(factory, parentObject, proxyShare)
+        {
+        }
 
-		///<param name="factory">current used factory core</param>
-		///<param name="parentObject">object there has created the proxy</param>
+        ///<param name="factory">current used factory core</param>
+        ///<param name="parentObject">object there has created the proxy</param>
         ///<param name="comProxy">inner wrapped COM proxy</param>
-		public DataSourceListener(Core factory, ICOMObject parentObject, object comProxy) : base(factory, parentObject, comProxy)
-		{
-			
-		}
+        public DataSourceListener(Core factory, ICOMObject parentObject, object comProxy) : base(factory, parentObject, comProxy)
+        {
+            
+        }
 
         ///<param name="parentObject">object there has created the proxy</param>
         ///<param name="comProxy">inner wrapped COM proxy</param>
         [EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
-		public DataSourceListener(ICOMObject parentObject, object comProxy) : base(parentObject, comProxy)
-		{
-		}
-		
-		///<param name="factory">current used factory core</param>
-		///<param name="parentObject">object there has created the proxy</param>
-        ///<param name="comProxy">inner wrapped COM proxy</param>
-        ///<param name="comProxyType">Type of inner wrapped COM proxy"</param>
-		[EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
-		public DataSourceListener(Core factory, ICOMObject parentObject, object comProxy, NetRuntimeSystem.Type comProxyType) : base(factory, parentObject, comProxy, comProxyType)
-		{
-
-		}
-
-		///<param name="parentObject">object there has created the proxy</param>
+        public DataSourceListener(ICOMObject parentObject, object comProxy) : base(parentObject, comProxy)
+        {
+        }
+        
+        ///<param name="factory">current used factory core</param>
+        ///<param name="parentObject">object there has created the proxy</param>
         ///<param name="comProxy">inner wrapped COM proxy</param>
         ///<param name="comProxyType">Type of inner wrapped COM proxy"</param>
         [EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
-		public DataSourceListener(ICOMObject parentObject, object comProxy, NetRuntimeSystem.Type comProxyType) : base(parentObject, comProxy, comProxyType)
-		{
-		}
-		
-		///<param name="replacedObject">object to replaced. replacedObject are not usable after this action</param>
-		[EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
-		public DataSourceListener(ICOMObject replacedObject) : base(replacedObject)
-		{
-		}
-		
-		[EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
-		public DataSourceListener() : base()
-		{
-		}
-		
-		/// <param name="progId">registered progID</param>
-		[EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
-		public DataSourceListener(string progId) : base(progId)
-		{
-		}
-		
-		#endregion
-		
-		#region Properties
+        public DataSourceListener(Core factory, ICOMObject parentObject, object comProxy, NetRuntimeSystem.Type comProxyType) : base(factory, parentObject, comProxy, comProxyType)
+        {
 
-		#endregion
+        }
 
-		#region Methods
+        ///<param name="parentObject">object there has created the proxy</param>
+        ///<param name="comProxy">inner wrapped COM proxy</param>
+        ///<param name="comProxyType">Type of inner wrapped COM proxy"</param>
+        [EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
+        public DataSourceListener(ICOMObject parentObject, object comProxy, NetRuntimeSystem.Type comProxyType) : base(parentObject, comProxy, comProxyType)
+        {
+        }
+        
+        ///<param name="replacedObject">object to replaced. replacedObject are not usable after this action</param>
+        [EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
+        public DataSourceListener(ICOMObject replacedObject) : base(replacedObject)
+        {
+        }
+        
+        [EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
+        public DataSourceListener() : base()
+        {
+        }
+        
+        /// <param name="progId">registered progID</param>
+        [EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
+        public DataSourceListener(string progId) : base(progId)
+        {
+        }
+        
+        #endregion
+        
+        #region Properties
 
-		/// <summary>
-		/// SupportByVersion MSDATASRC 4
-		/// </summary>
-		/// <param name="bstrDM">string bstrDM</param>
-		[EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
-		[SupportByVersion("MSDATASRC", 4)]
-		public Int32 dataMemberChanged(string bstrDM)
-		{
-			return Factory.ExecuteInt32MethodGet(this, "dataMemberChanged", bstrDM);
-		}
+        #endregion
 
-		/// <summary>
-		/// SupportByVersion MSDATASRC 4
-		/// </summary>
-		/// <param name="bstrDM">string bstrDM</param>
-		[EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
-		[SupportByVersion("MSDATASRC", 4)]
-		public Int32 dataMemberAdded(string bstrDM)
-		{
-			return Factory.ExecuteInt32MethodGet(this, "dataMemberAdded", bstrDM);
-		}
+        #region Methods
 
-		/// <summary>
-		/// SupportByVersion MSDATASRC 4
-		/// </summary>
-		/// <param name="bstrDM">string bstrDM</param>
-		[EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
-		[SupportByVersion("MSDATASRC", 4)]
-		public Int32 dataMemberRemoved(string bstrDM)
-		{
-			return Factory.ExecuteInt32MethodGet(this, "dataMemberRemoved", bstrDM);
-		}
+        /// <summary>
+        /// SupportByVersion MSDATASRC 4
+        /// </summary>
+        /// <param name="bstrDM">string bstrDM</param>
+        [EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
+        [SupportByVersion("MSDATASRC", 4)]
+        public Int32 dataMemberChanged(string bstrDM)
+        {
+            return Factory.ExecuteInt32MethodGet(this, "dataMemberChanged", bstrDM);
+        }
 
-		#endregion
+        /// <summary>
+        /// SupportByVersion MSDATASRC 4
+        /// </summary>
+        /// <param name="bstrDM">string bstrDM</param>
+        [EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
+        [SupportByVersion("MSDATASRC", 4)]
+        public Int32 dataMemberAdded(string bstrDM)
+        {
+            return Factory.ExecuteInt32MethodGet(this, "dataMemberAdded", bstrDM);
+        }
 
-		#pragma warning restore
-	}
+        /// <summary>
+        /// SupportByVersion MSDATASRC 4
+        /// </summary>
+        /// <param name="bstrDM">string bstrDM</param>
+        [EditorBrowsable(EditorBrowsableState.Never), Browsable(false)]
+        [SupportByVersion("MSDATASRC", 4)]
+        public Int32 dataMemberRemoved(string bstrDM)
+        {
+            return Factory.ExecuteInt32MethodGet(this, "dataMemberRemoved", bstrDM);
+        }
+
+        #endregion
+
+        #pragma warning restore
+    }
 }
